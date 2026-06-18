e5cffcab9e595994995f28d7d574883d5be4213d
By Dev King cuerbito <damianmoran2025@gmail.com>
2026-06-18 12:05:13 -0300

    Create settings.js

---
+++ settings.js
@@ -0,0 +1,37 @@
+import { watchFile, unwatchFile } from "fs";
+import chalk from "chalk";
+import { fileURLToPath } from "url";
+
+global.owner = ['5492644156919', '542646762285'];
+
+global.dev = "© ⍴᥆ᥕᥱrᥱძ ᑲᥡ ⁱᵃᵐ|Cuerbito";
+global.links = {
+  api: 'https://api.yuki-wabot.my.id',
+  channel: "https://whatsapp.com/channel/0029VbCq9xP2ZjCr6AGMzi1b",
+  github: "https://github.com/damianmoran2025-sketch/YukiBot-MD/edit/main/settings.js",
+  gmail: "damianmoran682@gmail.com"
+}
+global.my = {
+  ch1: '120363401404146384@newsletter'
+};
+
+global.APIs = { 
+  yuki: { url: "https://api.yuki-wabot.my.id", key: "YukiBot-MD" },
+  vreden: { url: "https://api.vreden.web.id", key: null },
+  ootaizumi: { url: "https://api.ootaizumi.web.id", key: null },
+  delirius: { url: "https://api.delirius.store", key: null },
+  zenzxz: { url: "https://api.zenzxz.my.id", key: null },
+  siputzx: { url: "https://app.siputzx.my.id", key: null }
+};
+
+global.mess = {
+  socket: '❇️ Este comando solo puede ser ejecutado por un Socket.',
+  admin: '❇️ Este comando solo puede ser ejecutado por los Administradores del Grupo.',
+  botAdmin: '❇️ Este comando solo puede ser ejecutado si el Socket es Administrador del Grupo.'
+};
+
+let file = fileURLToPath(import.meta.url);
+watchFile(file, () => {
+  unwatchFile(file);
+  import(`${file}?update=${Date.now()}`);
+});
